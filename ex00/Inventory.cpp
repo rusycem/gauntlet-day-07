8cdf6673a52a159df8d23c50f1b15ccac882ecf7
M. Irfan Rusydi <67940812+rusycem@users.noreply.github.com>
2026-08-15 18:47:55 +0800

    red: removing a missing item is a no-op
    read about tdd, need to update with red format accordingly
    
    tests.cpp:(.text+0x62): undefined reference to `Inventory::add(std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > const&)'
    /usr/bin/ld: tests.cpp:(.text+0x105): undefined reference to `Inventory::count() const'

--- ex00/Inventory.cpp
+++ ex00/Inventory.cpp
@@ -1,24 +1,2 @@
 #include "Inventory.hpp"
 #include <algorithm> //sort count etc
-
-void Inventory::add(const std::string& item)
-{
-    m_items.push_back(item);
-}
-
-int  Inventory::count() const
-{
-    return m_items.size();
-}
-
-bool Inventory::has(const std::string& item) const
-{
-    (void)item;
-    return false; //need to return bool
-}
-
-// absent item: no-op, cycle 4 proves it
-void Inventory::remove(const std::string& item)  
-{
-    (void)item;
-}
